17bf21eb9b824b3be901f9ce627356cc0e9efb27
zeyadelbanna <zeyadelbanna95@gmail.com>
2023-03-21 17:53:42 -0700

    nested_loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,9 +9,19 @@
  */
 int print_sign(int n)
 {
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	} else if (n == 0)
+        if (n > 0)
+        {
+        _putchar(43);
+        return (1);
+        }
+        else if (n < 0)
+        {
+        _putchar(45);
+        return (-1);
+        }
+        else
+        {
+        _putchar(48);
+        return (0);
+        }
 }
