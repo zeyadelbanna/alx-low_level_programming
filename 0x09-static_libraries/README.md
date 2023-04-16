bad8ff1debc874e168c0cf1c79f020267a74fd26
zeyadelbanna <104072449+zeyadelbanna@users.noreply.github.com>
2023-04-16 03:28:24 +0200

    libraries

--- 0x09-static_libraries/README.md
+++ 0x09-static_libraries/README.md
@@ -1,1 +1,1 @@
-readme
+# 0x09. C - Static libraries
--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+/**
+ * File: main
+ *
+ *Desc: header file
+ */
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -22,4 +26,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /*MAIN_H*/ 
+#endif
