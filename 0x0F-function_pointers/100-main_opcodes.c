82b7c38f7ff60d238ed29b8f69838bc361fbe226
zeyadelbanna <104072449+zeyadelbanna@users.noreply.github.com>
2023-04-22 04:29:14 +0200

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,28 +1,5 @@
-#include "function_pointers.h"
 #include <stdlib.h>
 #include <stdio.h>
-
-
-/**
-* print_opcodes - print the opcodes of this program
-* @a: address of the main function
-* @n: number of bytes to print
-*
-* Return: void
-*/
-void print_opcodes(char *a, int n)
-{
-   int i;
-
-
-   for (i = 0; i < n; i++)
-   {
-       printf("%02hhx", a[i]);
-       if (i < n - 1)
-           printf(" ");
-   }
-   printf("\n");
-}
 
 
 /**
@@ -32,22 +9,35 @@ void print_opcodes(char *a, int n)
 *
 * Return: always O
 */
-int main(int argc, char **argv)
-{
-   int n;
 
+int main(int argc, char *argv[])
+{
+   int bytes, index;
+   int (*address)(int,char **) = main;
+   unsigned char opcode;
 
    if (argc != 2)
    {
        printf("Error\n");
        exit(1);
    }
-   n = atoi(argv[1]);
-   if (n < 0)
+   
+   bytes = atoi(argv[1]);
+   
+   if (bytes < 0)
    {
        printf("Error\n");
        exit(2);
    }
-   print_opcodes((char *) &main, n);
+   for (index =0; index < bytes; index++)
+   {
+      opcode = *(unsigned char *)address;
+      printf("%.2x" , opcode);
+         if( index == bytes - 1)
+            continue;
+      print(" ");
+      address++;
+   }
+   printf("\n");
    return (0);
 }
