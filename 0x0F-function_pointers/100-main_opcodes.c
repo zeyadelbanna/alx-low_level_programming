39d73db634c7471bf28c5e77cd8c587df84d6ec4
zeyadelbanna <104072449+zeyadelbanna@users.noreply.github.com>
2023-04-22 03:59:23 +0200

    updates

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,14 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
-/**
-* print_opcodes - print the opcodes of this program
-* @a: address of the main function
-* @n: number of bytes to print
-*
-* Return: void
-*/
 void print_opcodes(char *a, int n)
 {
    int i;
@@ -23,14 +15,6 @@ void print_opcodes(char *a, int n)
    printf("\n");
 }
 
-
-/**
-* main - prints the opcodes of its own main function
-* @argc: number of arguments passed to the function
-* @argv: array of pointers to arguments
-*
-* Return: always O
-*/
 int main(int argc, char **argv)
 {
    int n;
@@ -41,12 +25,12 @@ int main(int argc, char **argv)
        printf("Error\n");
        exit(1);
    }
-   n = atoi(argv[1]);
-   if (n < 0)
+   x = atoi(argv[1]);
+   if (x < 0)
    {
        printf("Error\n");
        exit(2);
    }
-   print_opcodes((char *)&main, n);
+   print_opcodes((char *)&main, x);
    return (0);
 }
